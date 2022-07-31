06eb822cbc2d1fdfc4c4e6fcfba85247a27edf15
Kyle Larsen <kylelarsen.business@gmail.com>
2022-07-31 01:49:46 -0600

    add styling and java script function

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,69 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//
+var render = document.getElementById('generate');
+render.onclick = function() {
+    document.getElementById('inputBox').style.visibility = 'visible';
+    document.getElementById('inputBox').empty();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//
+var create = document.getElementById('begin');
+create.onclick = function() {
+    //
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var specialCharacters = "(<[{?!.*&%#$@;:,/~^+=-_}]>)";
+    var numberChart = "0123456789";
+    var characterType = "";
+    var passwordElements = "";
+    var passwordLength = document.getElementById('length').value;
+    var finishedProduct = document.querySelector('#passwordElements');
+    var selectedLength = document.querySelectorAll('input[type="checkbox"]:checked').length;
+
+    //
+    if (passwordLength >= 8 && passwordLength < 128) {
+        document.getElementById('inputBox').style.visibility = 'hidden';
+
+        //
+        if (document.getElementById('lowercase').checked) {
+            characterType += lowerCase;
+
+            passwordElements += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        }
+
+        //
+        if (document.getElementById('uppercase').checked) {
+            characterType += upperCase;
+            
+            passwordElements += upperCase[Math.floor(Math.random() * upperCase.length)];
+        }
+
+        //
+        if (document.getElementById('special-characters').checked) {
+            characterType += specialCharacters;
+
+            passwordElements += specialCharacters;[Math.floor(Math.random() * specialCharacters.length)];
+        }
+
+        //
+        if (document.getElementById('number-selector').checked) {
+            characterType += numberChart;
+
+            passwordElements += numberChart[Math.floor(Math.random() * numberChart.length)];
+        }
+
+        //
+        for (var i = 0; i < passwordLength - selectedLength; i++) {
+
+            passwordElements += characterType.charAt(Math.floor(Math.random() * characterType.length));
+
+            finishedProduct.value = passwordElements;
+        }
+
+        //
+        document.getElementById('errorPrompt').style.visibility = 'hidden';
+
+    } else {
+        //
+        document.getElementById('errorPrompt').style.visibility = 'visible';
+    }
+}
